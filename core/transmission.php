932aa3b89ca36710a097fe817bb86ada5b450ad2
Simon Lelorrain <slelorrain@gmail.com>
2014-10-22 19:56:10 +0200

    Refactoring of core/transmission

--- core/transmission.php
+++ core/transmission.php
@@ -2,46 +2,27 @@
 
 class Transmission{
 
-    public static function call( $function, $param = null ){
-        if( method_exists('Transmission', $function) ){
-            if( is_null($param) ){
-                $to_call = call_user_func('self::' . $function);
-            }else{
-                $to_call = call_user_func('self::' . $function, $param);
+    private static $options = array(
+		"add"           =>  '-a',
+		"listFiles"     =>  '-l',
+		"start"         =>  '--torrent all --start',
+		"stop"          =>  '--torrent all --stop',
+		"altSpeedOn"    =>  '-as',
+		"altSpeedOff"   =>  '-AS',
+		"info"          =>  '-si',
+	);
+
+    public static function call( $action, $param = null ){
+        if( array_key_exists($action, Transmission::$options) ){
+            $to_call = TRANSMISSION_CMD . ' ' . Transmission::$options[$action];
+            if( !is_null($param) ){
+                $to_call .= ' ' . $param;
             }
             $to_echo = Utils::execCommand($to_call);
         }else{
             $to_echo = 'Transmission action not found';
         }
         return $to_echo;
-    }
-
-    private static function add( $torrent ){
-        return TRANSMISSION_CMD . ' -a ' . $torrent;
-    }
-
-    private static function listFiles(){
-        return TRANSMISSION_CMD . ' -l';
-    }
-
-    private static function start(){
-        return TRANSMISSION_CMD . ' --torrent all --start';
-    }
-
-    private static function stop(){
-        return TRANSMISSION_CMD . ' --torrent all --stop';
-    }
-
-    private static function altSpeedOn(){
-        return TRANSMISSION_CMD . ' -as';
-    }
-
-    private static function altSpeedOff(){
-        return TRANSMISSION_CMD . ' -AS';
-    }
-
-    private static function info(){
-        return TRANSMISSION_CMD . ' -si';
     }
 
 }
