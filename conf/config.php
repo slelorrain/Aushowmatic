0d342dc912a38a5a9e0715ecbdb7fdc88c5d1ff0
PXgamer <owzie123@gmail.com>
2017-06-20 15:26:31 +0100

    PSR-2 update for config file

--- conf/config.php
+++ conf/config.php
@@ -3,19 +3,20 @@ date_default_timezone_set('Europe/Luxembourg');
 
 // User configuration
 $user_config_path = dirname(__FILE__) . '/user_config.json';
-if( file_exists($user_config_path) ){
+if (file_exists($user_config_path)) {
     $user_config = json_decode(file_get_contents($user_config_path));
-    foreach( $user_config as $key => $val ){
+    foreach ($user_config as $key => $val) {
         define($key, $val);
     }
 }
 
 // Default configuration
 $default_config_path = dirname(__FILE__) . '/default_config.json';
-if( file_exists($default_config_path) ){
+if (file_exists($default_config_path)) {
     $default_config = json_decode(file_get_contents($default_config_path));
-    foreach( $default_config as $key => $val ){
-        if( !defined($key) ) define($key, $val);
+    foreach ($default_config as $key => $val) {
+        if (!defined($key)) {
+            define($key, $val);
+        }
     }
 }
-?>
