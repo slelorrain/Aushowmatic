25e650ba6bb95650d4455daed3ff21c96ac6ef9a
PXgamer <owzie123@gmail.com>
2017-06-20 16:08:38 +0100

    Refactored and namespaced the Feeds classes

--- src/Feeds/EZTV.php
+++ src/Feeds/EZTV.php
@@ -1,52 +1,60 @@
 <?php
 
-class EZTV extends Feed{
+namespace slelorrain\Aushowmatic\Feeds;
+
+use slelorrain\Aushowmatic\Core;
+
+class EZTV extends Core\Feed
+{
 
     const PATH = "https://eztv.it/";
 
-    public static function getWebsiteLinkToShow( $show_id ){
+    public static function getWebsiteLinkToShow($show_id)
+    {
         return self::PATH . 'shows/' . $show_id;
     }
 
-    public static function getShowFeed( $show ){
+    public static function getShowFeed($show)
+    {
         return self::PATH . 'shows/' . $show;
     }
 
     // This parsing is crappy but the DOM is crappy too :p
-    public static function parsePage( $page, &$could_be_added, $use_min_date = true ){
-        $dom = new DomDocument();
+    public static function parsePage($page, &$could_be_added, $use_min_date = true)
+    {
+        $dom = new \DomDocument();
         // Warnings are muted because DOM retrieved is not valid
         libxml_use_internal_errors(true);
         $dom->loadHTML($page);
         libxml_clear_errors();
-        $finderEpinfo = new DomXPath($dom);
+        $finderEpinfo = new \DomXPath($dom);
 
         // Find lines corresponding to an episode
         $nodes = $finderEpinfo->query("//*[contains(@class, 'epinfo')]");
 
-        foreach( $nodes as $item ){
+        foreach ($nodes as $item) {
             // Date of released
             $released = $item->parentNode->nextSibling->nextSibling->nextSibling->nextSibling->nodeValue;
 
             // At the moment, for EZTV we only handle episodes with a release date below a week
-            if( !$use_min_date || $released != ">1 week" ){
+            if (!$use_min_date || $released != ">1 week") {
 
                 $episodeName = $item->nodeValue;
 
                 $infoLink = $item->parentNode->previousSibling->previousSibling->firstChild->nextSibling;
-                if( $infoLink->nodeType == XML_ELEMENT_NODE && $infoLink->hasAttribute('href') ){
+                if ($infoLink->nodeType == XML_ELEMENT_NODE && $infoLink->hasAttribute('href')) {
                     $infoLink = $infoLink->getAttribute('href');
-                }else{
+                } else {
                     $infoLink = $episodeName;
                 }
 
                 $downloadLink = $item->parentNode->nextSibling->nextSibling->firstChild->getAttribute('href');
 
-                if( !array_key_exists($infoLink, $could_be_added) ){
+                if (!array_key_exists($infoLink, $could_be_added)) {
                     $could_be_added[$infoLink] = $downloadLink;
-                }else{
+                } else {
                     // If current download link contains preferred format replace link previously set in array
-                    if( strpos($downloadLink, PREFERRED_FORMAT) !== false ){
+                    if (strpos($downloadLink, $_ENV['PREFERRED_FORMAT']) !== false) {
                         $could_be_added[$infoLink] = $downloadLink;
                     }
                 }
@@ -55,5 +63,3 @@ class EZTV extends Feed{
     }
 
 }
-
-?>
---
+++ src/Feeds/ShowRSS.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace slelorrain\Aushowmatic\Feeds;
+
+use slelorrain\Aushowmatic\Core;
+
+class ShowRSS extends Core\Feed
+{
+
+    const PATH = "http://showrss.info/";
+
+    public static function getWebsiteLinkToShow($show_id)
+    {
+        return self::PATH . 'browse/' . $show_id;
+    }
+
+    public static function getShowFeed($show_id)
+    {
+        return self::PATH . 'show/' . $show_id . '.rss';
+    }
+
+    public static function parsePage($page, &$could_be_added, $use_min_date = true)
+    {
+        libxml_use_internal_errors(true);
+        $xml = simplexml_load_string($page);
+        libxml_clear_errors();
+        if ($xml) {
+            foreach ($xml->channel->item as $item) {
+                if (!$use_min_date || strtotime($item->pubDate) >= strtotime(Core\Utils::getMinDate())) {
+
+                    $epId = "";
+                    $epTitle = $item->title;
+                    $downloadLink = $item->link;
+
+                    if (preg_match("(.+\s[0-9]+x[0-9]+)", $epTitle, $n)) {
+                        // 2x09
+                        $epId = $n[0];
+                    } else {
+                        if (preg_match("/([0-9]{4})-([0-9]{2})-([0-9]{2})/", $epTitle, $n)) {
+                            // 2014-10-20
+                            $epId = $n[0];
+                        }
+                    }
+
+                    if (!array_key_exists($epId, $could_be_added)) {
+                        $could_be_added[$epId] = $downloadLink;
+                    } else {
+                        // If current download link contains preferred format replace link previously set in array
+                        if (strpos($downloadLink, $_ENV['PREFERRED_FORMAT']) !== false) {
+                            $could_be_added[$epId] = $downloadLink;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
