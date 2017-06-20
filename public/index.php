e214e9da209ffc6567d4724ca541e08130c4b254
PXgamer <owzie123@gmail.com>
2017-06-20 17:21:10 +0100

    Reformatted the public/index

--- public/index.php
+++ public/index.php
@@ -8,6 +8,7 @@ use slelorrain\Aushowmatic\Core\Utils;
 new Aushowmatic\Config();
 
 Core\Dispatcher::dispatch();
+$isTurtleActivated = Core\System::isTurtleActivated();
 ?>
 <!DOCTYPE html>
 <html>
@@ -29,52 +30,75 @@ Core\Dispatcher::dispatch();
 
 <div id="remote">
     <ul class="yt-button-group">
-        <li><a href="?a=transmission&param=stop" class="yt-button" title="Stop all torrents">&#9632;</a></li>
-        <li><a href="?a=transmission&param=start" class="yt-button" title="Start all torrents">&#9658;</a></li>
+        <li>
+            <a href="?a=transmission&param=stop"
+               class="yt-button" title="Stop all torrents">&#9632;</a>
+        </li>
+        <li>
+            <a href="?a=transmission&param=start"
+               class="yt-button" title="Start all torrents">&#9658;</a>
+        </li>
     </ul>
     <ul class="yt-button-group">
-        <?php $isTurtleActivated = Core\System::isTurtleActivated(); ?>
-        <li><a href="?a=transmission&param=altSpeedOn" class="yt-button <?php if ($isTurtleActivated) {
-                echo 'active';
-            } ?>" title="Turtle ON">Turtle</a></li>
-        <li><a href="?a=transmission&param=altSpeedOff" class="yt-button <?php if (!$isTurtleActivated) {
-                echo 'active';
-            } ?>" title="Turtle OFF">&infin;</a></li>
+        <li>
+            <a href="?a=transmission&param=altSpeedOn"
+               class="yt-button <?= $isTurtleActivated ? 'active' : '' ?>" title="Turtle ON">Turtle</a>
+        <li>
+            <a href="?a=transmission&param=altSpeedOff"
+               class="yt-button  <?= !$isTurtleActivated ? 'active' : '' ?>" title="Turtle OFF">&infin;</a>
+        </li>
     </ul>
     <ul class="yt-button-group">
-        <li><a href="?a=transmission&param=listFiles" class="yt-button" title="List torrents">&equiv;</a></li>
-        <li><a href="?a=transmission&param=info" class="yt-button" title="Info">&iexcl;</a></li>
-        <li><a href="<?= $_ENV['TRANSMISSION_WEB'] ?>" target="_blank" class="yt-button" title="Transmission Web Interface">TWI</a></li>
+        <li>
+            <a href="?a=transmission&param=listFiles"
+               class="yt-button" title="List torrents">&equiv;</a>
+        </li>
+        <li>
+            <a href="?a=transmission&param=info"
+               class="yt-button" title="Info">&iexcl;</a>
+        </li>
+        <li>
+            <a href="<?= $_ENV['TRANSMISSION_WEB'] ?>" target="_blank"
+               class="yt-button" title="Transmission Web Interface">TWI</a>
+        </li>
     </ul>
 </div>
 
 <div id="main_container" class="auto">
 
-    <?php if (!is_writable($_ENV['FEED_INFO'])): ?>
+    <?php if (!is_writable($_ENV['FEED_INFO'])) { ?>
         <div class="alert">The feed file is not writable. Please update permissions.</div>
-    <?php endif; ?>
+    <?php } ?>
 
     <nav>
         <ul class="yt-button-group left">
-            <li><a href="?a=done" class="yt-button <?php if (isset($_GET['a']) && $_GET['a'] == 'done') {
-                    echo 'active';
-                } ?>">Processed links</a></li>
-            <li><a href="?a=shows" class="yt-button <?php if (isset($_GET['a']) && $_GET['a'] == 'shows') {
-                    echo 'active';
-                } ?>">Added shows</a></li>
+            <li>
+                <a href="?a=done"
+                   class="yt-button <?= isset($_GET['a']) && $_GET['a'] == 'done' ? 'active' : '' ?>">Processed links</a>
+            </li>
+            <li>
+                <a href="?a=shows"
+                   class="yt-button <?= isset($_GET['a']) && $_GET['a'] == 'shows' ? 'active' : '' ?>">Added shows</a>
+            </li>
         </ul>
         <ul class="yt-button-group left">
-            <li><a id="add_show" href="#add_show" class="yt-button">Add a show</a></li>
-            <li><a id="add_torrent" href="#add_torrent" class="yt-button">Add a torrent</a></li>
+            <li>
+                <a id="add_show" href="#add_show" class="yt-button">Add a show</a>
+            </li>
+            <li>
+                <a id="add_torrent" href="#add_torrent" class="yt-button">Add a torrent</a>
+            </li>
         </ul>
 
         <ul class="yt-button-group right">
-            <li><a href="?a=preview" class="yt-button <?php if (isset($_GET['a']) && $_GET['a'] == 'preview') {
-                    echo 'active';
-                } ?>">Preview downloads</a></li>
-            <li><a href="?a=launch" class="yt-button primary <?php if (isset($_GET['a']) && $_GET['a'] == 'launch') {
-                    echo 'active';
-                } ?>">Launch downloads</a></li>
+            <li>
+                <a href="?a=preview"
+                   class="yt-button <?= isset($_GET['a']) && $_GET['a'] == 'done' ? 'preview' : '' ?>">Preview downloads</a>
+            </li>
+            <li>
+                <a href="?a=launch"
+                   class="yt-button primary <?= isset($_GET['a']) && $_GET['a'] == 'launch' ? 'active' : '' ?>">Launch downloads</a>
+            </li>
         </ul>
         <div class="clear"></div>
 
@@ -90,16 +114,14 @@ Core\Dispatcher::dispatch();
         </form>
     </nav>
 
-    <pre id="response"><?php if (isset($_SESSION['result'])) {
-            echo $_SESSION['result'];
-        } ?></pre>
+    <pre id="response"><?= isset($_SESSION['result']) ? $_SESSION['result'] : '' ?></pre>
 
     <div id="bottom_links">
         <div class="left">
             <a href="?a=updateDate" class="yt-button">Update min. date</a>
             <a href="?a=emptyDone" class="yt-button danger">Empty processed links</a>
         </div>
-        <?php if ($_ENV['SYSTEM_CMDS_ENABLED'] == 'true'): ?>
+        <?php if ($_ENV['SYSTEM_CMDS_ENABLED'] == 'true') { ?>
             <div class="right">
                 <a href="?a=diskUsage" class="yt-button">Disk space usage</a>
                 <ul class="yt-button-group">
@@ -114,13 +136,17 @@ Core\Dispatcher::dispatch();
                     <a href="?a=shutdown" class="yt-button danger big">Shutdown</a>
                 </div>
             </div>
-        <?php endif; ?>
+        <?php } ?>
         <div class="clear"></div>
     </div>
 
 </div>
 
-<footer>Min. date : <?= Utils::getMinDate() ?> / Generated in <?= $_SESSION['generated_in'] ?>s</footer>
+<footer>
+    <span>Min. date : <?= Utils::getMinDate() ?></span>
+    <span> / </span>
+    <span>Generated in <?= $_SESSION['generated_in'] ?>s</span>
+</footer>
 
 <script src="assets/main.js"></script>
 
