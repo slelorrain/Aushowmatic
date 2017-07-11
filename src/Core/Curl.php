3d59bc1aa9a61a5dfcf454307ba243b230e6f4ae
Simon Lelorrain <slelorrain@eurodns.com>
2017-07-11 22:52:58 +0200

    Extract Curl functions

---
+++ src/Core/Curl.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace slelorrain\Aushowmatic\Core;
+
+class Curl
+{
+
+    public static function getPage($url)
+    {
+        if (isset($url)) {
+            $ch = self::getCurlHandle($url);
+            $page = curl_exec($ch);
+            curl_close($ch);
+            return $page;
+        }
+    }
+
+    public static function getPages($urls)
+    {
+        if (isset($urls)) {
+            return self::doCurlMulti($urls);
+        }
+    }
+
+    private static function getCurlHandle($url)
+    {
+        if (isset($url)) {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($ch, CURLOPT_MAXREDIRS, 1);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+
+            return $ch;
+        }
+    }
+
+    // Create cURL handles, add them to a multi handle, and then run them in parallel
+    // Based on the example of http://php.net/manual/en/function.curl-multi-exec.php
+    //
+    // On php 5.3.18+, curl_multi_select() may return -1 forever until you call curl_multi_exec()
+    // See https://bugs.php.net/bug.php?id=63411 for more information
+    private static function doCurlMulti($urls)
+    {
+        // Create a new cURL multi handle
+        $mh = curl_multi_init();
+
+        // Get and add handles
+        foreach ($urls as $url) {
+            $ch = self::getCurlHandle($url);
+            curl_multi_add_handle($mh, $ch);
+            $handles[] = $ch;
+        }
+
+        // Process each of the handles in the stack
+        $active = null;
+        do {
+            $mrc = curl_multi_exec($mh, $active);
+        } while ($mrc == CURLM_CALL_MULTI_PERFORM);
+
+        do {
+            curl_multi_select($mh); // non-busy wait for state change
+            $mrc = curl_multi_exec($mh, $active); // get new state
+        } while ($active);
+
+        // Retrieve the content of cURL handles and remove them
+        foreach ($handles as $ch) {
+            $pages[] = curl_multi_getcontent($ch);
+            curl_multi_remove_handle($mh, $ch);
+        }
+
+        // Close the cURL multi handle
+        curl_multi_close($mh);
+
+        return $pages;
+    }
+
+}
--- src/Core/Feed.php
+++ src/Core/Feed.php
@@ -12,13 +12,11 @@ abstract class Feed implements FeedInterface
 
         if (isset($show) && !empty($show)) {
             // Retrieve content of show page
-            $ch = self::getCurlHandle($show);
-            $page = curl_exec($ch);
-            curl_close($ch);
+            $page = Curl::getPage(static::getShowFeed($show));
             static::parsePage($page, $could_be_added, false);
         } else {
             // Retrieve content of pages
-            $pages = self::doCurlMulti();
+            $pages = Curl::getPages(Utils::getShowList());
             // Parse pages and retrieve links that could be added
             foreach ($pages as $page) {
                 static::parsePage($page, $could_be_added);
@@ -33,61 +31,6 @@ abstract class Feed implements FeedInterface
         }
 
         return $added;
-    }
-
-    private static function getCurlHandle($show)
-    {
-        if (isset($show)) {
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, static::getShowFeed($show));
-            curl_setopt($ch, CURLOPT_HEADER, 0);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-            return $ch;
-        }
-    }
-
-    // Create cURL handles, add them to a multi handle, and then run them in parallel
-    // Based on the example of http://php.net/manual/en/function.curl-multi-exec.php
-    //
-    // On php 5.3.18+, curl_multi_select() may return -1 forever until you call curl_multi_exec()
-    // See https://bugs.php.net/bug.php?id=63411 for more information
-    private static function doCurlMulti()
-    {
-
-        // Create a new cURL multi handle
-        $mh = curl_multi_init();
-
-        // Get and add handles
-        foreach (Utils::getShowList() as $show) {
-            if (!empty($show)) {
-                $ch = self::getCurlHandle($show);
-                curl_multi_add_handle($mh, $ch);
-                $handles[] = $ch;
-            }
-        }
-
-        // Process each of the handles in the stack
-        $active = null;
-        do {
-            $mrc = curl_multi_exec($mh, $active);
-        } while ($mrc == CURLM_CALL_MULTI_PERFORM);
-
-        do {
-            curl_multi_select($mh); // non-busy wait for state change
-            $mrc = curl_multi_exec($mh, $active); // get new state
-        } while ($active);
-
-        // Retrieve the content of cURL handles and remove them
-        foreach ($handles as $ch) {
-            $pages[] = curl_multi_getcontent($ch);
-            curl_multi_remove_handle($mh, $ch);
-        }
-
-        // Close the cURL multi handle
-        curl_multi_close($mh);
-
-        return $pages;
     }
 
 }
