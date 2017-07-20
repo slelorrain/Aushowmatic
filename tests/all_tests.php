6b730e450f4c784f3b56bbcc83747533bf2b5581
Simon Lelorrain <slelorrain@gmail.com>
2017-07-20 20:37:52 +0200

    Correct all_tests.php

--- tests/all_tests.php
+++ tests/all_tests.php
@@ -12,7 +12,7 @@ class AllTests extends TestSuite
 
     public function __construct()
     {
-        parent::__construct('All tests for Aushowmatic ' . Utils::getVersion());
+        parent::__construct('All tests for Aushowmatic ' . Aushowmatic\Core\Utils::getVersion());
         $this->addFile(dirname(__FILE__) . '/FeedInfo_test.php');
     }
 }
