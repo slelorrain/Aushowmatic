95080b3a0793d3edaf6bdbbacf44af2899a697a2
Simon Lelorrain <slelorrain@eurodns.com>
2018-05-30 12:08:51 +0200

    Add Resolution

---
+++ src/Core/Resolution.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace slelorrain\Aushowmatic\Core;
+
+class Resolution implements ChoosableInterface
+{
+
+    public static function getChoices()
+    {
+        return array('720p', '1080p');
+    }
+
+}
